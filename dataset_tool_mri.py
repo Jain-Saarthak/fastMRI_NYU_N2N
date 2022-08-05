5f95c0a6f2f999d01896548eee9c010b3d137672
Jain-Saarthak <saarthakjain01@gmail.com>
2022-08-05 07:50:41 -0700

    added natural image N2N model

--- dataset_tool_mri.py
+++ dataset_tool_mri.py
@@ -12,29 +12,36 @@ import argparse
 import glob
 import os
 import PIL.Image
+import PIL.ImageOps
 import numpy as np
 import sys
 
 import util
 
-import nibabel as nib
+# import nibabel as nib
 
 OUT_RESOLUTION = 256
 
 # Select z-slices from [25,124]
-slice_min = 25
-slice_max = 125
+slice_min = 1
+slice_max = 2
 
 # Select train and validation subsets from IXI-T1 (these two lists shouldn't overlap)
-train_basenames=['IXI002-Guys-0828', 'IXI012-HH-1211', 'IXI013-HH-1212', 'IXI014-HH-1236', 'IXI015-HH-1258', 'IXI016-Guys-0697', 'IXI017-Guys-0698', 'IXI019-Guys-0702', 'IXI020-Guys-0700', 'IXI021-Guys-0703', 'IXI022-Guys-0701', 'IXI023-Guys-0699', 'IXI024-Guys-0705', 'IXI025-Guys-0852', 'IXI026-Guys-0696', 'IXI027-Guys-0710', 'IXI028-Guys-1038', 'IXI029-Guys-0829', 'IXI030-Guys-0708', 'IXI031-Guys-0797', 'IXI033-HH-1259', 'IXI034-HH-1260', 'IXI035-IOP-0873', 'IXI036-Guys-0736', 'IXI037-Guys-0704', 'IXI038-Guys-0729', 'IXI039-HH-1261', 'IXI040-Guys-0724', 'IXI041-Guys-0706', 'IXI042-Guys-0725', 'IXI043-Guys-0714', 'IXI044-Guys-0712', 'IXI045-Guys-0713', 'IXI046-Guys-0824', 'IXI048-HH-1326', 'IXI049-HH-1358', 'IXI050-Guys-0711', 'IXI051-HH-1328', 'IXI052-HH-1343', 'IXI053-Guys-0727', 'IXI054-Guys-0707', 'IXI055-Guys-0730', 'IXI056-HH-1327', 'IXI057-HH-1342', 'IXI058-Guys-0726', 'IXI059-HH-1284', 'IXI060-Guys-0709', 'IXI061-Guys-0715', 'IXI062-Guys-0740', 'IXI063-Guys-0742']
-valid_basenames=['IXI064-Guys-0743', 'IXI065-Guys-0744', 'IXI066-Guys-0731', 'IXI067-HH-1356', 'IXI068-Guys-0756', 'IXI069-Guys-0769', 'IXI070-Guys-0767', 'IXI071-Guys-0770', 'IXI072-HH-2324', 'IXI073-Guys-0755']
+train_basenames=[]
+valid_basenames=['file_brain_AXT1_202_2020477_0']
+# train_basenames=['IXI002-Guys-0828', 'IXI012-HH-1211', 'IXI013-HH-1212', 'IXI014-HH-1236', 'IXI015-HH-1258', 'IXI016-Guys-0697', 'IXI017-Guys-0698', 'IXI019-Guys-0702', 'IXI020-Guys-0700', 'IXI021-Guys-0703', 'IXI022-Guys-0701', 'IXI023-Guys-0699', 'IXI024-Guys-0705', 'IXI025-Guys-0852', 'IXI026-Guys-0696', 'IXI027-Guys-0710', 'IXI028-Guys-1038', 'IXI029-Guys-0829', 'IXI030-Guys-0708', 'IXI031-Guys-0797', 'IXI033-HH-1259', 'IXI034-HH-1260', 'IXI035-IOP-0873', 'IXI036-Guys-0736', 'IXI037-Guys-0704', 'IXI038-Guys-0729', 'IXI039-HH-1261', 'IXI040-Guys-0724', 'IXI041-Guys-0706', 'IXI042-Guys-0725', 'IXI043-Guys-0714', 'IXI044-Guys-0712', 'IXI045-Guys-0713', 'IXI046-Guys-0824', 'IXI048-HH-1326', 'IXI049-HH-1358', 'IXI050-Guys-0711', 'IXI051-HH-1328', 'IXI052-HH-1343', 'IXI053-Guys-0727', 'IXI054-Guys-0707', 'IXI055-Guys-0730', 'IXI056-HH-1327', 'IXI057-HH-1342', 'IXI058-Guys-0726', 'IXI059-HH-1284', 'IXI060-Guys-0709', 'IXI061-Guys-0715', 'IXI062-Guys-0740', 'IXI063-Guys-0742']
+# valid_basenames=['IXI064-Guys-0743', 'IXI065-Guys-0744', 'IXI066-Guys-0731', 'IXI067-HH-1356', 'IXI068-Guys-0756', 'IXI069-Guys-0769', 'IXI070-Guys-0767', 'IXI071-Guys-0770', 'IXI072-HH-2324', 'IXI073-Guys-0755']
 
 def fftshift2d(x, ifft=False):
     assert (len(x.shape) == 2) and all([(s % 2 == 1) for s in x.shape])
     s0 = (x.shape[0] // 2) + (0 if ifft else 1)
     s1 = (x.shape[1] // 2) + (0 if ifft else 1)
+    print("s0", s0)
+    print("s1", s1)
     x = np.concatenate([x[s0:, :], x[:s0, :]], axis=0)
+    print("x", x)
     x = np.concatenate([x[:, s1:], x[:, :s1]], axis=1)
+    print("x", x)
     return x
 
 def preprocess_mri(input_files,
@@ -59,14 +66,25 @@ def preprocess_mri(input_files,
     for i, fn in enumerate(all_files):
         if i % 100 == 0:
             print('%d / %d ..' % (i, num_images))
-        img = np.asarray(PIL.Image.open(fn), dtype=np.uint8)
-        img_primal[i] = img
+        image = PIL.Image.open(fn)
+        image = PIL.ImageOps.grayscale(image)
 
+        img = np.asarray(image, dtype=np.uint8)
+        print("img", img)
+        img_primal[i] = img
+        print("i", i)
+        print("img_primal", img_primal)
+        print("img_primal[i]", img_primal[i])
         img = img.astype(np.float32) / 255.0 - 0.5
         img = img[crop_slice]
+        print("img", img)
         spec = np.fft.fft2(img).astype(np.complex64)
+        print("spec", spec)
         spec = fftshift2d(spec)
+        print("spec", spec)
         img_spectrum[i] = spec
+        print("img_spectrum[i]", img_spectrum[i])
+        print("img_spectrum", img_spectrum)
 
     print('Saving: %s' % output_file)
     util.save_pkl((img_primal, img_spectrum), output_file)
@@ -115,7 +133,7 @@ def genpng(args):
                 PIL.Image.fromarray(output).convert('L').save(outname)
 
 def make_slice_name(basename, slice_idx):
-    return basename + ('-T1_%03d.png' % slice_idx)
+    return basename + ('.png')
 
 def genpkl(args):
     if args.png_dir is None:
@@ -127,15 +145,15 @@ def genpkl(args):
 
     input_train_files = []
     input_valid_files = []
-    for base in train_basenames:
-        for sidx in range(slice_min, slice_max):
-            input_train_files.append(os.path.join(args.png_dir, make_slice_name(base, sidx)))
+    # for base in train_basenames:
+    #     for sidx in range(slice_min, slice_max):
+    #         input_train_files.append(os.path.join(args.png_dir, make_slice_name(base, sidx)))
     for base in valid_basenames:
         for sidx in range(slice_min, slice_max):
             input_valid_files.append(os.path.join(args.png_dir, make_slice_name(base, sidx)))
-    print ('Num train samples', len(input_train_files))
+    # print ('Num train samples', len(input_train_files))
     print ('Num valid samples', len(input_valid_files))
-    preprocess_mri(input_files=input_train_files, output_file=os.path.join(args.pkl_dir, 'ixi_train.pkl'))
+    # preprocess_mri(input_files=input_train_files, output_file=os.path.join(args.pkl_dir, 'ixi_train.pkl'))
     preprocess_mri(input_files=input_valid_files, output_file=os.path.join(args.pkl_dir, 'ixi_valid.pkl'))
 
 def extract_basenames(lst):
